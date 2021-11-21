fc2011c6098ef06c8dea83249e0190d64d01b27a
ranstar74 <34216771+ranstar74@users.noreply.github.com>
2021-11-21 14:03:42 +0300

    Add basic air brakes with steam engine

---
+++ AdvancedTrainSystem/Railroad/Components/SteamComponents/AirbrakeComponent.cs
@@ -0,0 +1,32 @@
+﻿using AdvancedTrainSystem.Core.Components.Abstract;
+using RageComponent.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedTrainSystem.Railroad.Components.SteamComponents
+{
+    public class AirbrakeComponent : BrakeComponent
+    {
+        public override float Intensity => intensity;
+
+        private float intensity;
+
+        public AirbrakeComponent(ComponentCollection components) : base(components)
+        {
+
+        }
+
+        public override void Start()
+        {
+            base.Start();
+        }
+
+        public override void Update()
+        {
+
+        }
+    }
+}
---
+++ AdvancedTrainSystem/Railroad/Components/SteamComponents/SteamEngineComponent.cs
@@ -0,0 +1,35 @@
+﻿using AdvancedTrainSystem.Core.Components.Abstract;
+using RageComponent.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedTrainSystem.Railroad.Components.SteamComponents
+{
+    public class SteamEngineComponent : EngineComponent
+    {
+        private ControlsComponent controls;
+        private BoilerComponent boiler;
+        public SteamEngineComponent(ComponentCollection components) : base(components)
+        {
+
+        }
+
+        public override void Start()
+        {
+            base.Start();
+
+            controls = Components.GetComponent<ControlsComponent>();
+            boiler = Components.GetComponent<BoilerComponent>();
+        }
+
+        public override void Update()
+        {
+            float engineOutput = controls.Throttle * boiler.Pressure;
+
+            GTA.UI.Screen.ShowSubtitle($"{engineOutput:0.0}");
+        }
+    }
+}
